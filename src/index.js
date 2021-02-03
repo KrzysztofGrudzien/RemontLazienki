1d05db5be16b9859e0c094c485402b2baea61636
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-02-03 13:56:56 +0100

    set up a fundamental structure and config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App/App';
+import './styles/index.scss';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
