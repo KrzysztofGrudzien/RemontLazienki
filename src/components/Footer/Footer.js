d20ad312369207b3e2aba3433ce847e251fb42e0
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-02-07 01:58:33 +0100

    add content and style for footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,13 @@
 import footerStyles from './Footer.module.scss';
 
 const Footer = () => {
-    return <footer className={footerStyles.footer}>Footer</footer>;
+    return (
+        <footer className={footerStyles.footer}>
+            <p className={footerStyles.footerCopyrights}>
+                Wszelkie prawa zastrzeżone przez Artur Grudzień - 2021 &copy;
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
