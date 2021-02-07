4c8cc5ab2ce692660ecd969c758925fdf7eeed72
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-02-07 01:57:57 +0100

    add content and style for sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,29 @@
 import sidebarStyles from './Sidebar.module.scss';
+import IconPhone from '../../assets/icons/icon-phone.svg';
+import IconMail from '../../assets/icons//icon-envelop.svg';
+import IconFacebook from '../../assets/icons/icon-fb.svg';
 
 const Sidebar = () => {
-    return <div className={sidebarStyles.sidebar}>sidebar</div>;
+    return (
+        <div className={sidebarStyles.sidebar}>
+            <div className={sidebarStyles.sidebarIcons}>
+                <img src={IconPhone} alt='icon of phone.' />
+                <img src={IconMail} alt='icon of email.' />
+                <img src={IconFacebook} alt='icon of facebook.' />
+            </div>
+            <p className={sidebarStyles.sidebarAuthor}>
+                Projekt i wdrożenie{' '}
+                <a
+                    className={sidebarStyles.sidebarLink}
+                    href='http://criscode.eu'
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    Krzysztof Grudzień
+                </a>
+            </p>
+        </div>
+    );
 };
 
 export default Sidebar;
