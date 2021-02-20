e2e3c51c7cb5ea8572b76e042579e42cbced31ce
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-02-20 16:26:14 +0100

    add react router and test pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,28 +3,41 @@ import Sidebar from '../Sidebar/Sidebar';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
 import appStyles from './App.module.scss';
-// import PageAbout from '../../pages/About/About';
-// import PageServices from '../../pages/Services/Services';
-// import Page404 from '../../pages/404/404';
-// import PageContact from '../../pages/Contact/Contact';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PageAbout from '../../pages/About/About';
+import PageServices from '../../pages/Services/Services';
+import Page404 from '../../pages/404/404';
+import PageContact from '../../pages/Contact/Contact';
 import PageGallery from '../../pages/Gallery/Gallery';
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Sidebar />
             <div className={appStyles.container}>
                 <Header />
                 <Main>
-                    {/* <PageAbout></PageAbout> */}
-                    {/* <PageServices></PageServices> */}
-                    {/* <Page404 /> */}
-                    {/* <PageContact /> */}
-                    <PageGallery />
+                    <Switch>
+                        <Route path='/galeria'>
+                            <PageGallery />
+                        </Route>
+                        <Route path='/kontakt'>
+                            <PageContact />
+                        </Route>
+                        <Route path='/404'>
+                            <Page404 />
+                        </Route>
+                        <Route path='/uslugi'>
+                            <PageServices />
+                        </Route>
+                        <Route path='/'>
+                            <PageAbout />
+                        </Route>
+                    </Switch>
                 </Main>
                 <Footer />
             </div>
-        </>
+        </Router>
     );
 };
 
