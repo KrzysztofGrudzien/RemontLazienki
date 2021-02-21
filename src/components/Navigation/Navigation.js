5f07422a62c4a47cd38851726e903e3965832498
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-02-21 05:48:33 +0100

    add content of navigation, styles and routing

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,74 @@
 import navigationStyles from './Navigation.module.scss';
+import IconPhone from '../../assets/icons/icon-phone.svg';
+import IconEnvelop from '../../assets/icons/icon-envelop.svg';
+import IconFacebook from '../../assets/icons/icon-fb.svg';
+import { Link } from 'react-router-dom';
+import TemplateNavigationLink from '../Templates/TemplateNavigationLink/TemplateNavigationLink';
 
 const Navigation = () => {
     return (
-        <nav className={navigationStyles.navbar}>
-            <div></div>
-        </nav>
+        <>
+            <nav className={navigationStyles.navbar}>
+                <ul className={navigationStyles.navbarList}>
+                    <li className={navigationStyles.navbarItem}>
+                        <TemplateNavigationLink>
+                            <Link
+                                to='/'
+                                className={navigationStyles.navbarItemLink}
+                            >
+                                o nas
+                            </Link>
+                        </TemplateNavigationLink>
+                    </li>
+                    <li className={navigationStyles.navbarItem}>
+                        <TemplateNavigationLink>
+                            <Link
+                                to='/uslugi'
+                                className={navigationStyles.navbarItemLink}
+                            >
+                                usługi
+                            </Link>
+                        </TemplateNavigationLink>
+                    </li>
+                    <li className={navigationStyles.navbarItem}>
+                        <TemplateNavigationLink>
+                            <Link
+                                to='/galeria'
+                                className={navigationStyles.navbarItemLink}
+                            >
+                                galeria
+                            </Link>
+                        </TemplateNavigationLink>
+                    </li>
+                    <li className={navigationStyles.navbarItem}>
+                        <TemplateNavigationLink>
+                            <Link
+                                to='/kontakt'
+                                className={navigationStyles.navbarItemLink}
+                            >
+                                kontakt
+                            </Link>
+                        </TemplateNavigationLink>
+                    </li>
+                </ul>
+                <div className={navigationStyles.navbarDetail}>
+                    <img src={IconPhone} alt='phone icon' />
+                    519-811-460
+                </div>
+                <div className={navigationStyles.navbarDetail}>
+                    <img src={IconEnvelop} alt='email icon' />
+                    <a href='#' className={navigationStyles.navbarDetailLink}>
+                        dekorilla@wp.pl
+                    </a>
+                </div>
+                <div className={navigationStyles.navbarDetail}>
+                    <img src={IconFacebook} alt='facebook icon' />
+                    <a href='#' className={navigationStyles.navbarDetailLink}>
+                        facebook.com/dekorilla
+                    </a>
+                </div>
+            </nav>
+        </>
     );
 };
 
